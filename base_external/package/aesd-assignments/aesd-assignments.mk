181473a2ccb4c4eee63b17c624f7657a187393b4
DeanGineer <Deniel.Molina.Careers@gmail.com>
2025-02-23 01:08:39 +0800

    Added server to buildroot make

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'ffbb65c5909032ca37e232d36ad207716176874c'
+AESD_ASSIGNMENTS_VERSION = 'e28c440bbc50a393ae46a8db650a2a632c84b64d'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -30,6 +30,9 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/usr/bin/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/usr/bin/conf/
+	$(INSTALL) -d 0755 $(@D)/server/ $(TARGET_DIR)/root/
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/root/
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
